c3517198316ab827a8e5d6a04be07c46c85a66cd
pemex69 <randomtrtrandom@gmail.com>
2024-04-29 10:40:47 -0600

    queries fiz

--- server/queries/users.js
+++ server/queries/users.js
@@ -1,18 +1,11 @@
 const getUsers = 'SELECT * FROM public."Usuario"';
 const checkEmailExists =
-  'SELECT s FROM "Users_schema"."Users" s WHERE s.usr_email = $1';
+  'SELECT s FROM public."Usuario" s WHERE s.correo = $1';
 const addUser =
-  'INSERT INTO "Users_schema"."Users" (usr_username, usr_email, usr_pass) VALUES ($1, $2, $3)';
+  'INSERT INTO public."Usuario" (nombre, correo, password) VALUES ($1, $2, $3)';
 
 export default {
   getUsers,
   checkEmailExists,
   addUser,
 };
-
-// module.exports = {
-//   getUsers,
-//   checkEmailExists,
-//   addUser,
-// };
-// export every variable as default
